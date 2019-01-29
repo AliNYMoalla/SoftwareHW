222f1bdcf2bfad2c7aecc6e35dca299772524b98
ali_m <ali_m@192.168.1.106>
2019-01-29 16:32:03 +0200

    HW Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+public class HelloWorld {
+	
+public static void printtext(String x) {
+	  JOptionPane.showMessageDialog(null,x );
+}
+	public static void main(String[] args) {
+	String x="Hello World";
+	printtext(x);
+	}
+
+}
